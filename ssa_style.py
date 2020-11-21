be774d5d4ae73f82211d044bd15ae570a5073503
Lily <pixielf.0@gmail.com>
2020-11-21 01:48:25 -0500

    add validation for SSA style

--- ssa_style.py
+++ ssa_style.py
@@ -1,5 +1,6 @@
 import dataclasses
 import enum
+import warnings
 
 
 class BorderStyle(enum.Enum):
@@ -15,6 +16,17 @@ class Alignment(enum.IntEnum):
     BOTTOM = 0b0000
     MIDDLE = 0b1000
     TOP = 0b0100
+
+    @classmethod
+    def validate(cls, value: int):
+        """ Determine if the value is a possible alignment. Returns True when valid; raises ValueError otherwise. """
+        for x in (cls.LEFT, cls.CENTER, cls.RIGHT):
+            for y in (cls.BOTTOM, cls.MIDDLE, cls.TOP):
+                if x | y == value:
+                    return True
+
+        # no valid (x, y) combination gets to this value
+        raise ValueError(f'{value} is not a valid alignment.')
 
 
 @dataclasses.dataclass
@@ -61,6 +73,87 @@ class Style:
     # Specifies the font character set or encoding and on multilingual Windows installations of SSA, it provides
     # access to characters used in more than one language. It is usually zero for English (Western, ANSI) Windows.
     encoding: int = 0
+
+    @staticmethod
+    def _validate_colour(c):
+        """ Validate/parse the input color. """
+        if isinstance(c, str):
+            # Try to interpret as a hex color (#)RRGGBB
+            try:
+                original = c
+                c = int(c.lstrip('#'), 16)
+                warnings.warn(f'color {original!r} given, interpreted as {c}')
+            except ValueError:
+                raise TypeError('color {c!r} given, not interpretable as hex color')
+
+        if not isinstance(c, int):
+            # Try to interpret as an int (perhaps float?)
+            try:
+                original = c
+                c = int(c)
+                warnings.warn(f'color {original!r} given, interpreted as {c}')
+            except ValueError:
+                raise TypeError('color {c!r} given, not interpretable as color')
+
+        # For integer values, check if it's in the correct range.
+        if not 0 <= c <= 0xFFFFFF:
+            original = c
+            c = c & 0xFFFFFF
+            warnings.warn(f'color {original} == #{original:06X} given, outside [0, 0xFFFFFF]. Using {c} instead.')
+
+        return c
+
+    def __post_init__(self):
+        """ Check that all of the values are valid. """
+        if not isinstance(self.name, str):
+            raise TypeError(f'Style.name must be a str, not {self.name!r}')
+
+        if not isinstance(self.font_name, str):
+            raise TypeError(f'Style.font_name must be a str, not {self.name!r}')
+
+        if not isinstance(self.font_size, int):
+            try:
+                s = self.font_size
+                self.font_size = int(self.font_size)
+                warnings.warn(f'Passed {s!r} as Style.font_size. Converted to {self.font_size!r}')
+            except ValueError:
+                raise TypeError(f'Style.font_size must be interpretable as int, not {self.font_size}')
+
+        for colour in (self.primary_colour, self.secondary_colour, self.outline_colour, self.back_colour):
+            self._validate_colour(colour)
+
+        if not isinstance(self.bold, bool):
+            s = self.bold
+            self.bold = bool(s)
+            warnings.warn(f'Passed {s!r} as Style.bold. Converted to {self.bold!r}')
+
+        if not isinstance(self.italic, bool):
+            s = self.italic
+            self.italic = bool(s)
+            warnings.warn(f'Passed {s!r} as Style.italic. Converted to {self.italic!r}')
+
+        if not isinstance(self.underline, bool):
+            s = self.underline
+            self.underline = bool(s)
+            warnings.warn(f'Passed {s!r} as Style.underline. Converted to {self.underline!r}')
+
+        if not isinstance(self.strikethrough, bool):
+            s = self.strikethrough
+            self.strikethrough = bool(s)
+            warnings.warn(f'Passed {s!r} as Style.strikethrough. Converted to {self.strikethrough!r}')
+
+        # If self.border_style isn't already an BorderStyle, it is converted to one, raising ValueError
+        # if the border_style value is invalid.
+        self.border_style = BorderStyle(self.border_style)
+
+        # outline and shadow must be one of 0, 1, 2, 3, 4
+        if self.outline not in range(5):
+            raise ValueError(f'Style.outline must be one of 0, 1, 2, 3, 4, not {self.outline!r}')
+
+        if self.shadow not in range(5):
+            raise ValueError(f'Style.shadow must be one of 0, 1, 2, 3, 4, not {self.shadow!r}')
+
+        Alignment.validate(self.alignment)
 
     @classmethod
     def header(cls):
